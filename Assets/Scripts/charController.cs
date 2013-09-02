e2ca407fc1026127caf54d40794bc0e96074c9a8
mholtkamp <mholt012@gmail.com>
2013-09-01 20:34:26 -0400

    Basic Project

---
+++ Assets/Scripts/charController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charController : MonoBehaviour {
+	
+	//Character Related
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {	
+		speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Handle Input
+		if(Input.GetKey(KeyCode.A))
+			transform.Translate(-speed*Time.deltaTime,0f,0f);
+		if(Input.GetKey(KeyCode.D))
+			transform.Translate(speed*Time.deltaTime,0f,0f);
+		if(Input.GetKey(KeyCode.W))
+			transform.Translate(0f,0f,speed*Time.deltaTime);
+		if(Input.GetKey(KeyCode.S))
+			transform.Translate(0f,0f,-speed*Time.deltaTime);
+			
+	}
+}
