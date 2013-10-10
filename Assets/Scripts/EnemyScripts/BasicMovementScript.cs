0e5ed43b9704b2b008d0c396aa2172710aa84d33
mholtkamp <mholt012@gmail.com>
2013-10-10 00:18:12 -0400

    Recreated BasicMovementScript
    After the tragic accidental asset folder deletion :(

---
+++ Assets/Scripts/EnemyScripts/BasicMovementScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovementScript : MonoBehaviour {
+	
+	GameObject chara;
+	private bool targetAcquired;
+	private Vector3 targVect;
+	private RaycastHit colCast;
+	Stats stats;
+	float collisionBuffer = 0.001f;
+	float speed;
+	
+	void Start () {
+		chara = GameObject.Find ("Character");
+		targetAcquired = false;
+		targVect = new Vector3();
+		colCast = new RaycastHit();
+		stats = (Stats) GetComponent (typeof(Stats));
+		
+	}
+	
+	void Update () {
+		
+		search();
+		if(targetAcquired)
+			move();
+		transform.LookAt (chara.transform);
+		animate();
+
+	}
+	
+	private void search()
+	{
+		targVect.x = chara.transform.position.x - transform.position.x;
+		targVect.z = chara.transform.position.z - transform.position.z;
+		if(targetAcquired)
+			return;
+		else
+		{
+			Physics.Raycast (transform.position, targVect, out colCast,targVect.magnitude);
+			if(colCast.transform.gameObject == chara)
+			{
+				targetAcquired = true;
+				print ("TARGET FOUND");
+			}
+		}
+	}
+	
+	private void move()
+	{
+		speed = (float) (stats.speed/100f);
+			if(Physics.SphereCast(transform.position,(collider as SphereCollider).bounds.size.x/2,new Vector3(Mathf.Sign (targVect.x),0,0),out colCast,speed*Time.deltaTime))
+				transform.Translate (Mathf.Sign(targVect.x)*colCast.distance + -1f*Mathf.Sign(targVect.x)*collisionBuffer,0f,0f,Space.World);
+			else
+				transform.Translate((targVect.x/targVect.magnitude)*speed*Time.deltaTime,0f,0f,Space.World);
+		
+			if(Physics.SphereCast(transform.position,(collider as SphereCollider).bounds.size.x/2,new Vector3(0,0,Mathf.Sign (targVect.z)),out colCast,speed*Time.deltaTime))
+				transform.Translate (0f,0f,Mathf.Sign(targVect.z)*colCast.distance + -1f*Mathf.Sign(targVect.z)*collisionBuffer,Space.World);
+			else
+				transform.Translate(0f,0f,(targVect.z/targVect.magnitude)*speed*Time.deltaTime,Space.World);
+		
+	}
+	
+	private void animate()
+	{
+		if(!targetAcquired)
+			animation.Play ("Idle");
+		else
+			animation.Play ("Move");
+	}
+}
---
+++ Assets/Scripts/EnemyScripts/TridentGhost/TridentGhostController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TridentGhostController : MonoBehaviour {
+	Stats stats;
+	// Use this for initialization
+	void Start () {
+		stats = (Stats) GetComponent(typeof(Stats));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				stats.speed = 100;
+
+	}
+}
