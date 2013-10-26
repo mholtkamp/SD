baf9a4054171a6a6b19b78bb09ece6ad5e4cd036
mholtkamp <mholt012@gmail.com>
2013-10-25 20:46:24 -0400

    More additions to title screen
    Castle now floats.
    Improved class select button texture qualities.

--- Assets/Scripts/Title/cameraScript.cs
+++ Assets/Scripts/Title/cameraScript.cs
@@ -5,6 +5,10 @@ public class cameraScript : MonoBehaviour {
 	startButtonScript start;
 	exitButtonScript exit;
 	titleScript title;
+	wizardButtonScript wiz;
+	warriorButtonScript war;
+	archerButtonScript arc;
+	classTextScript cts;
 	int state ;
 	
 	//State 1 vars
@@ -16,11 +20,19 @@ public class cameraScript : MonoBehaviour {
 	float xrotdiff;
 	float yrotdiff;
 	
+	//state 4 vars
+	Vector3 source;
+	Vector3 dest;
+	
 	// Use this for initialization
 	void Start () {
 		start = GameObject.Find ("Start").GetComponent (typeof(startButtonScript)) as startButtonScript;
 		exit = GameObject.Find ("Exit").GetComponent (typeof(exitButtonScript)) as exitButtonScript;
 		title = GameObject.Find ("Title").GetComponent (typeof(titleScript)) as titleScript;
+		wiz = GameObject.Find ("WizardButton").GetComponent (typeof(wizardButtonScript)) as wizardButtonScript;
+		war = GameObject.Find ("WarriorButton").GetComponent (typeof(warriorButtonScript)) as warriorButtonScript;
+		arc = GameObject.Find ("ArcherButton").GetComponent (typeof(archerButtonScript)) as archerButtonScript;
+		cts = GameObject.Find ("ClassText").GetComponent(typeof(classTextScript)) as classTextScript;
 		state = 0;
 	}
 
@@ -64,15 +76,59 @@ public class cameraScript : MonoBehaviour {
 			}
 			else
 			{
-				
+				wiz.show();
+				arc.show();
+				war.show();
+				cts.show();
+
 				state = 3;
 			}
 		}
 		else if(state == 3)
 		{
+			if(wiz.faded && war.faded && arc.faded && cts.faded)
+			{
+				Destroy (wiz.gameObject);
+				Destroy (war.gameObject);
+				Destroy (arc.gameObject);
+				Destroy (cts.gameObject);
+				
+				source = transform.position;
+				dest = new Vector3(83.6f,13.8f,-68.5f);
+				xrotdiff = 352.8f - transform.localEulerAngles.x;
+				yrotdiff = 91.8f - transform.localEulerAngles.y;
+				state = 4;
+			}
+		}
+		else if(state == 4)
+		{
+			
+			Vector3 newRot = transform.localEulerAngles;
+			Vector3 newPos = transform.position;
+			if((transform.position.x > 83.6f) || (transform.position.y < 13.8f) || (transform.position.z > -68.5f))
+				repoCam (dest,source);
+			if((transform.localEulerAngles.x < 352.8f) || (transform.localEulerAngles.y < 91.7f))
+			{
+				if((transform.localEulerAngles.x < 352.8f))
+					newRot.x += xrotdiff*Time.deltaTime;
+				if((transform.localEulerAngles.y < 91.7f))
+					newRot.y += yrotdiff*Time.deltaTime;
+				
+				if(newRot.x > 352.8f)
+					newRot.x = 352.9f;
+				if(newRot.y > 91.7f)
+					newRot.y = 91.8f;
+				
+				transform.localEulerAngles = newRot;
+			}
+			else
+			{
+				state = 5;
+			}
 			
 			
 			
+
 		}
 		
 		
@@ -107,5 +163,12 @@ public class cameraScript : MonoBehaviour {
 		state = 2;
 	
 		
+	}
+	
+	
+	void repoCam(Vector3 dest, Vector3 source)
+	{
+		Vector3 diff = (dest - source)*Time.deltaTime;
+		transform.Translate (diff,Space.World);
 	}
 }
--- Assets/Scripts/Title/castleScript.cs
+++ Assets/Scripts/Title/castleScript.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class castleScript : MonoBehaviour {
 
-	// Use this for initialization
+	Vector3 pos,newPos;
+	float time;
+	public float amp;
 	void Start () {
-	
+		pos = new Vector3();
+		newPos = new Vector3();
+		pos.x = transform.position.x;
+		pos.y = transform.position.y;
+		pos.z = transform.position.z;
+		time = 0f;
+		amp = 0.1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		time += Time.deltaTime;
+		newPos.x = pos.x;
+		newPos.y = pos.y;
+		newPos.z = pos.z;
+		newPos.y += Mathf.Sin (time);
+		transform.position = newPos;
 	
 	}
 }
